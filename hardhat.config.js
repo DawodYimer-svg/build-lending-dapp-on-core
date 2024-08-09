7f48a514b56d7638366356d7a0cfe5c40e4254bc
DawodYimer <dawodyimer678@gmail.com>
2024-08-09 18:11:03 +0300

    code update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-verify");
 
-
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -14,18 +13,19 @@ module.exports = {
     core_testnet: {
       url: "https://rpc.test.btcs.network",
       accounts: privateKey(),
-    }
+    },
   },
   solidity: {
-    version: "0.8.24",
+    version: "0.8.19",
     settings: {
-      evmVersion: 'paris',
+      evmVersion: "paris",
       optimizer: {
         enabled: true,
         runs: 1000,
       },
     },
-  },  etherscan: {
-    apiKey: process.env.API_KEY, 
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
   },
 };
--- ignition/modules/Deploy.js
+++ ignition/modules/Deploy.js
@@ -1,1 +1,13 @@
-// Add your deployment script
+// Add your deployment script for Helloworld project
+/*
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+module.exports = buildModule("LoanDAppBasicModule", (m) => {
+  const Contract = m.contract("CoreLoanPlatform", []);
+  return { Contract };
+});
+*/
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+module.exports = buildModule("CoreLoanPlatform", (m) => {
+  const CoreLoanPlatform = m.contract("CoreLoanPlatform");
+  return { CoreLoanPlatform };
+});
